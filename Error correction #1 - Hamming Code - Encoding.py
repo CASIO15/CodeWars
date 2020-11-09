76dd25b7a980ae2484da04d79213597dfad91fe3
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2020-11-09 03:08:38 +0200

    Update Error correction #1 - Hamming Code - Encoding.py

--- Error correction #1 - Hamming Code - Encoding.py
+++ Error correction #1 - Hamming Code - Encoding.py
@@ -1,9 +1,8 @@
 from string import punctuation
 
-def encoding_(sting):
+
+def encoding_(string):
     
-    str_lst = []
-    changed_special = []
     final_result = []
 
     for i in string:
@@ -15,13 +14,14 @@ def encoding_(sting):
 
             for i in final_bin[3:]:
                 for x in i:
-                    final_result.append(x*3)
+                    final_result.append(x * 3)
         else:
             raw_str = bin(ord(i)).replace('b', '')
             for i in raw_str:
-                final_result.append(i*3)
+                final_result.append(i * 3)
 
     return ''.join(final_result)
+
 
 encoding_('T?st!%')
 # Result = 000111000111000111000000000000111111111111111111000111111111000000111111000111111111000111000000000000111000000000000111000000111000000111000111
