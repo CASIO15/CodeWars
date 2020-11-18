76c63383fe6c4e0e6bc60524db980d6d1c3c1be4
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2020-11-19 00:50:19 +0200

    Update Simple_reversed_parenthesis.py

--- Simple_reversed_parenthesis.py
+++ Simple_reversed_parenthesis.py
@@ -1,6 +1,6 @@
 def solve(string):
 
-    res = []
+    raw = []
 
     index = 0
     final = 0
@@ -9,23 +9,23 @@ def solve(string):
         return -1
 
     else:
-        for i in range(len(string)):
-            new_str = filter(lambda x: x != '()', string.split('()'))
+        for _ in range(len(string)):
+            new_str = string.split('()')
             string = ''.join(new_str)
 
     for _ in range(len(string)):
-        res.append(string[index:index+2])
+        raw.append(string[index:index+2])
         index += 2
 
-    for i in res:
-        if i != '':
-            if i == ')(':
-                final += 2
+    for i in raw:
+        if i == ')(':
+            final += 2
 
-            elif i == '))' or i == '((':
-                final += 1
+        elif i == '))' or i == '((':
+            final += 1
 
     return final
+    
     
 print(solve("())()))))()()(")) # 4
 print(solve('())(((')) # 3
